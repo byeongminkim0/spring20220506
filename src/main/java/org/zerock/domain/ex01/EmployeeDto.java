ebd1eff30dfdddbbd0da620976a48780b2a7bbac
kbm <99157638+byeongminkim0@users.noreply.github.com>
2022-05-11 16:34:03 +0900

    0511 spring(DTO)

---
+++ src/main/java/org/zerock/domain/ex01/EmployeeDto.java
@@ -0,0 +1,14 @@
+package org.zerock.domain.ex01;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+@Data
+public class EmployeeDto {
+	private String firstName;
+	private String lastName;
+	private String birthDate;
+	private String photo;
+	private String notes;
+}
