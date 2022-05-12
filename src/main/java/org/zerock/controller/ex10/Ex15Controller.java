918745c8bd2786f9b654327ca2b443e62b09ce10
kbm <99157638+byeongminkim0@users.noreply.github.com>
2022-05-12 15:23:52 +0900

    0512 spring(Controller)

---
+++ src/main/java/org/zerock/controller/ex10/Ex15Controller.java
@@ -0,0 +1,46 @@
+package org.zerock.controller.ex10;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.domain.ex02.BoardDto;
+import org.zerock.service.ex03.Ex05Service;
+
+@Controller
+@RequestMapping("ex15")
+public class Ex15Controller {
+	
+	@Autowired
+	private Ex05Service service;
+	
+	@RequestMapping("sub01")
+	public String method01(int id, Model model) {
+		String name = service.getCustomerNameById(id);
+		
+		model.addAttribute("name", name);
+		
+		return "/ex14/sub01";
+	}
+	
+	@RequestMapping("sub02")
+	public String method02(int id, Model model) {
+		String name = service.getEmployeeFirstNameById(id);
+		
+		model.addAttribute("employeeName", name);
+		
+		return "ex14/sub02";
+	}
+	
+	// /ex15/board/list
+	@GetMapping("board/list")
+	public void listBoard(Model model) {
+		List<BoardDto> list = service.listBoard();
+		
+		model.addAttribute("boardList", list);
+		
+	}
+}
