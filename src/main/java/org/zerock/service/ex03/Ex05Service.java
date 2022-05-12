eacc45240312ca767d3001f6bf0080b50ce21a01
kbm <99157638+byeongminkim0@users.noreply.github.com>
2022-05-12 15:23:37 +0900

    0512 spring(mapper xml)

---
+++ src/main/java/org/zerock/service/ex03/Ex05Service.java
@@ -0,0 +1,28 @@
+package org.zerock.service.ex03;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.zerock.domain.ex02.BoardDto;
+import org.zerock.mapper.ex03.Ex03Mapper;
+
+@Service
+public class Ex05Service {
+
+	@Autowired
+	private Ex03Mapper mapper;
+	
+	public String getCustomerNameById(int id) {
+		return mapper.selectCustomerNameById(id);
+	}
+
+	public String getEmployeeFirstNameById(int id) {
+		return mapper.selectEmployeeFirstNameById(id);
+	}
+
+	public List<BoardDto> listBoard() {
+		return mapper.selectBoard();
+	}
+
+}
